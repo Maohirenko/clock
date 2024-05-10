80d3a8d9fa82832d5b7a74a56981ca0ac437549d
Mykhailo Ohirenko <misha@MacBook-Air--Mykhailo.local>
2024-05-10 19:29:40 +0300

    header fix

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -22,7 +22,9 @@ export default function Header() {
                         <LanguageSwticherComponent />
                     </div>
                 </div>
-                <h1 className={classes.appHeading}>{t('headerTitle')}</h1>
+                <div className={classes.appHeading}>
+                <h1 className={classes.appHeadingText}>{t('headerTitle')}</h1>
+                </div>
             {/* </div> */}
             <div className={classes.navLinks}>
                 <Link className={`${pathname === '/analogue' ? classes.activeLink : classes.link} ${isModalShown ? classes.disabledButton : null}`} to={'/analogue'}>
