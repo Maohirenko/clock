cec76c4f5eb8318dc26dab9b419165fed8ae8993
Mykhailo Ohirenko <misha@MacBook-Air--Mykhailo.local>
2024-06-06 06:27:20 +0300

    formatting

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,9 +2,9 @@ import classes from "./footer.module.css";
 
 export default function Footer() {
     return (
-            <div className={classes.footerContainer}>
-                <span className={classes.copyright}>Copyright &#169;</span>
-                <span>Mykhailo Ohirenko 2024</span>
-            </div>
+        <div className={classes.footerContainer}>
+            <span className={classes.copyright}>Copyright &#169;</span>
+            <span>Mykhailo Ohirenko 2024</span>
+        </div>
     )
 }
