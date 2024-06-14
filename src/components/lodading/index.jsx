20c5cb0ac36ba4151e850f1b969f04d056cdd7cc
unknown <mishaogione01@gmail.com>
2024-06-14 22:10:50 +0300

    loading screen and icon update

--- src/components/lodading/index.jsx
+++ src/components/lodading/index.jsx
@@ -3,7 +3,8 @@ import classes from "./loading.module.css"
 export default function Loading() {
     return (
         <div className={classes.loadingContainer}>
-            <h1 className={classes.loadingText}>Keep waiting, the time is coming</h1>
+            <h1 className={classes.loadingText}>Keep waiting please, the app is loading...</h1>
+            <h2 className={classes.loadingText}>Зачекайте будь ласка, програма завантажується...</h2>
         </div>
     )
 }
